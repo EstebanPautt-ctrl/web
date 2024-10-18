d35416532ba0dc418c510fb7cc92f8b88f998b7c
EstebanPautt-ctrl <estebandaniel.pauttmontt@unitecnar.edu.co>
2024-10-18 17:30:08 -0500

    web

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Añade las extensiones que uses en tu proyecto
+    "**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
